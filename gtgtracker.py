57c9423978bf83fad9d9519f71c7ff71027d7d03
CCShoop <caelantennis@gmail.com>
2023-10-17 00:47:38 -0400

    last direct main commit

--- gtgtracker.py
+++ gtgtracker.py
@@ -38,22 +38,47 @@ def generatePlayersString(PLAYERS):
 def tallyScores(PLAYERS):
     print('Tallying scores')
     winners = []
-    winner = PLAYERS[0]
+    results = []
+    scoreboard = []
+    decreasingPLAYERS = PLAYERS.copy()
+    lowestRemainingPlayer = decreasingPLAYERS[0]
+    winner = decreasingPLAYERS[0]
 
-    for player in PLAYERS:
+    for player in decreasingPLAYERS:
         if player.completed and player.score < winner.score:
             winner = player
     winners.append(winner)
-    for player in PLAYERS:
+    for player in decreasingPLAYERS:
         if player.completed and player.name != winner.name and player.score == winner.score:
             winners.append(player)
 
     for winner in winners:
         winner.wins += 1
+
+    for player in decreasingPLAYERS:
+        if player.score <= lowestRemainingPlayer.score:
+            lowestRemainingPlayer = player
+            scoreboard.append(lowestRemainingPlayer)
+            decreasingPLAYERS.remove(player)
+
+    placeCounter = 1
+    for player in scoreboard:
+        print(f'{placeCounter}. {player.name} ({player.wins}) with score of {player.score}')
+        if player in winners:
+            results.append(f'{placeCounter}. {player.name} ({player.wins}) wins by guessing the game in {player.score} guesses!\n')
+            placeCounter -= 1
+        elif player.score < 6:
+            results.append(f'{placeCounter}. {player.name} ({player.wins}) guessed the game in {player.score} guesses.\n')
+        else:
+            results.append(f'{placeCounter}. {player.name} ({player.wins}) did not successfully guess the game.\n')
+        placeCounter += 1
+
     for player in PLAYERS:
         player.score = 0
         writeFile(replaceFileLines('PLAYERS', generatePlayersString(PLAYERS)))
     SCORED = True
+
+    return results
 
 
 load_dotenv()
@@ -107,7 +132,7 @@ async def register_command(interaction):
     PLAYERS.append(playerObj)
     playersString = ''
     for player in PLAYERS:
-        playersString += f'{player.name},'
+        playersString += f'{player.name}{NAME_DELIMITER}0{SCORE_DELIMITER}0{PLAYER_DELIMITER}'
     writeFile(replaceFileLines('PLAYERS', playersString))
     os.putenv('PLAYERS', playersString)
     await interaction.response.send_message('You have been registered for GuessTheGame tracking.')
@@ -115,25 +140,32 @@ async def register_command(interaction):
 @tree.command(name='deregister', description='Deregister for GuessTheGame tracking. WARNING: YOUR WINS WILL BE PERMANANTLY LOST!')
 async def deregister_command(interaction):
     channel = client.get_channel(int(CHANNEL))
+    removed = False
     for player in PLAYERS:
         if player.name == interaction.user.name.strip():
             PLAYERS.remove(player)
+            playersString = ''
+            for player in PLAYERS:
+                playersString += f'{player.name}{NAME_DELIMITER}0{SCORE_DELIMITER}0{PLAYER_DELIMITER}'
+            writeFile(replaceFileLines('PLAYERS', playersString))
+            os.putenv('PLAYERS', playersString)
             print(f'Deregistered user {player.name}')
             await channel.send(f'You have been deregistered for GuessTheGame tracking.')
-            return
-    print(f'Unregistered user {interaction.user.name.strip()} attempted to deregister')
-    await channel.send(f'You were already unregistered for GuessTheGame tracking.')
+            removed = True
+    if not removed:
+        print(f'Unregistered user {interaction.user.name.strip()} attempted to deregister')
+        await channel.send(f'You were already unregistered for GuessTheGame tracking.')
 
 
 @tasks.loop(hours=20)
 async def midnight_call():
+    channel = client.get_channel(int(CHANNEL))
     if not SCORED:
         shamed = ''
         for player in PLAYERS:
             if not player.completed:
                 user = discord.utils.get(client.users, name=player.name)
                 shamed += f'{user.mention} '
-        channel = client.get_channel(int(CHANNEL))
         await channel.send(f'SHAME ON {shamed} FOR NOT ATTEMPTING TO GUESS THE GAME!')
         tallyScores(PLAYERS)
         SCORED = False
@@ -142,7 +174,6 @@ async def midnight_call():
         player.completed = False
         user = discord.utils.get(client.users, name=player.name)
         everyone += f'{user.mention} '
-    channel = client.get_channel(int(CHANNEL))
     await channel.send(f'{everyone} it\'s time to Guess The Game!')
 
 
@@ -159,7 +190,22 @@ async def on_message(message):
         return
 
     # someone posts their GTG results
+    channel = client.get_channel(int(CHANNEL))
     if '#GuessTheGame' in message.content:
+        user = discord.utils.get(client.users, name=message.author.name)
+        # there are no registered players
+        if not PLAYERS:
+            await channel.send(f'{user.mention}, there are no registered players! Please register and resend your results to be the first.')
+            return
+
+        # player is not registered
+        found = False
+        for iPlayer in PLAYERS:
+            if message.author.name == iPlayer.name:
+                found = True
+        if not found:
+            await channel.send(f'{message.author.name}, you are not registered! Please register and resend your results.')
+
         unplayed = ''
         player = PLAYERS[0]
         found = False
@@ -169,18 +215,17 @@ async def on_message(message):
                 found = True
                 player = iPlayer
             elif not iPlayer.completed:
-                user = discord.utils.get(client.users, name=iPlayer.name)
-                unplayed += f'{user.mention} '
+                unplayedUser = discord.utils.get(client.users, name=iPlayer.name)
+                unplayed += f'{unplayedUser.mention} '
         if not found:
             print('Unregistered player posted a result')
-            channel = client.get_channel(int(CHANNEL))
-            user = discord.utils.get(client.users, name=message.author.name)
             await channel.send(f'Unregistered user {user.mention} attempted to post a result. Please register with "/register" and try again.')
             return
         print(f'Received GTG message from {message.author}')
 
         result = message.content.splitlines()[2].replace(' ', '')[1:]
         print('Result line: ' + result)
+        player.score = 0
         for char in result:
             if char == '🟥':
                 player.score += 1
@@ -194,10 +239,10 @@ async def on_message(message):
         print('Added reaction to message')
 
         if unplayed == '':
-            tallyScores(PLAYERS)
+            for score in tallyScores(PLAYERS):
+                await channel.send(score)
         else:
             unplayed += 'Guess The Game!'
-            channel = client.get_channel(int(CHANNEL))
             await channel.send(unplayed)
             pass
     else:
